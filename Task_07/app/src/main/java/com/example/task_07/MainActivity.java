1accbe2a9f40f7f1d0a14b0fd76e36fd7bd37974
211dbwls <211dbwls@naver.com>
2022-03-17 21:16:55 +0900

    과제07 파일 이름 수정

--- Task_07/app/src/main/java/com/example/task_07/MainActivity.java
+++ Task_07/app/src/main/java/com/example/task_07/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_main);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
