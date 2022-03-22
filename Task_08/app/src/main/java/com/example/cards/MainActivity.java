5424023311b5b7491918461fb394c6dfdc37e1a3
211dbwls <211dbwls@naver.com>
2022-03-23 00:51:28 +0900

    과제08 Cards - 눌린 버튼 알아내기

--- Task_08/app/src/main/java/com/example/cards/MainActivity.java
+++ Task_08/app/src/main/java/com/example/cards/MainActivity.java
@@ -2,10 +2,22 @@ package com.example.cards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int[] BUTTON_IDS = new int[] {  // 버튼 id
+            R.id.card_00, R.id.card_01, R.id.card_02,R.id.card_03,
+            R.id.card_10, R.id.card_11, R.id.card_12,R.id.card_13,
+            R.id.card_20, R.id.card_21, R.id.card_22,R.id.card_23,
+            R.id.card_30, R.id.card_31, R.id.card_32,R.id.card_33
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +25,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onBtnRestart(View view) {
+        Log.d(TAG, "Restart");
+    }
+
     public void onBtnCard(View view) {
+        if(!(view instanceof ImageButton)) {  // view가 ImageButton이 아닌 경우 예외 처리
+            Log.d(TAG, "Not an ImageButton: " + view);
+            return;
+        }
+        ImageButton imageButton = (ImageButton) view;
+
+        int cardIndex = findButtonIndex(imageButton.getId());  // 카드 인덱스 얻어옴.
+        Log.d(TAG, "Card: " + cardIndex);
+
+    }
+
+    private int findButtonIndex(int id) {  // 카드 인텍스 리턴하는 함수.
+        for(int i = 0; i < BUTTON_IDS.length; ++i) {
+            if(id == BUTTON_IDS[i]) {
+                    return i;
+            }
+        }
+        return -1;
     }
 }
