e6d82955fdc3e5cf57362a9bc5edc4ea166cf885
211dbwls <211dbwls@naver.com>
2022-04-01 00:55:17 +0900

    과제10 MoreControls - DoIt 버튼 클릭시 EditText에 입력된 내용 출력되도록

--- Task_10/app/src/main/java/com/example/morecontrols/MainActivity.java
+++ Task_10/app/src/main/java/com/example/morecontrols/MainActivity.java
@@ -6,15 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    CheckBox cb;
+    EditText et;
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cb = findViewById(R.id.checkbox);
+        et = findViewById(R.id.nameEdit);
+        tv = findViewById(R.id.outputTextView);
     }
 
     public void onCheckBox(View view) {
@@ -23,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnDoIt(View view) {
-        CheckBox cb = findViewById(R.id.checkbox);
         Log.d(TAG, "onBtnDoIt(), Checked: " + cb.isChecked());
+
+        String text = et.getText().toString();
+        tv.setText(text);
     }
+
 }
