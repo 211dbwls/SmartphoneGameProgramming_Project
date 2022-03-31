8e0a911e02b5c30e206c736bacab3c18ec1d85dc
211dbwls <211dbwls@naver.com>
2022-04-01 01:51:19 +0900

    과제10 MoreControls - 축구공 그리기

--- Task_10/app/src/main/java/com/example/morecontrols/GameView.java
+++ Task_10/app/src/main/java/com/example/morecontrols/GameView.java
@@ -1,9 +1,14 @@
 package com.example.morecontrols;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +18,7 @@ public class GameView extends View {
     private static final String TAG = GameView.class.getSimpleName();
     
     private Paint paint;
+    private Bitmap soccerBitmap;
 
     public GameView(Context context) {
         super(context);
@@ -31,12 +37,16 @@ public class GameView extends View {
     private void initView() {
         paint = new Paint();
         paint.setColor(Color.parseColor("#FFE4E1"));
+
+        Resources res = getResources();
+        soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         // super.onDraw(canvas);
 
+        /* RoundRect 그리기 */
         int left = getPaddingLeft();
         int top = getPaddingTop();
         int right = getPaddingRight();
@@ -48,5 +58,19 @@ public class GameView extends View {
         canvas.drawRoundRect(left, top, width - right, height - bottom, rx, ry, paint);
 
         Log.d(TAG, "size: " + width +  ", " + height + " padding: " + left + ", " + top + ", " + right + ", " + bottom);
+
+        /* 축구공 그리기 */
+        int cx = left + (width - left - right) / 2;
+        int cy = top + (height - top - bottom) / 2;
+        int ballRadius = width / 10;
+
+        Rect src = new Rect(0, 0, soccerBitmap.getWidth(), soccerBitmap.getHeight());
+        RectF dst = new RectF();
+        dst.left = cx - ballRadius;
+        dst.top = cy - ballRadius;
+        dst.right = cx + ballRadius;
+        dst.bottom = cy + ballRadius;
+
+        canvas.drawBitmap(soccerBitmap, src, dst, null);
     }
 }
