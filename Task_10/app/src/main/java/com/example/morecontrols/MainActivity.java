6a84b56e8e65c71041d67d8a7bc6e241f7041b1f
211dbwls <211dbwls@naver.com>
2022-04-01 01:03:44 +0900

    과제10 MoreControls - TextWatcher 사용

--- Task_10/app/src/main/java/com/example/morecontrols/MainActivity.java
+++ Task_10/app/src/main/java/com/example/morecontrols/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.morecontrols;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -25,7 +27,27 @@ public class MainActivity extends AppCompatActivity {
         cb = findViewById(R.id.checkbox);
         et = findViewById(R.id.nameEdit);
         tv = findViewById(R.id.outputTextView);
+
+        et.addTextChangedListener(editWatcher);
     }
+
+    TextWatcher editWatcher = new TextWatcher() {    // TextWatcher 세번째 방법
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {  // 입력하기 전에 호출
+            Log.v(TAG, "before");
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {  // 변화가 있을 때 호출
+            Log.d(TAG, "text change: " + charSequence);
+            tv.setText("Text Length: " + charSequence.length());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {  // 입력 끝난 후 호출
+            Log.v(TAG, "after");
+        }
+    };
 
     public void onCheckBox(View view) {
         CheckBox cb = (CheckBox) view;
