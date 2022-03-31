a1c7f10b5b2f40345abf65bf444208a921976588
211dbwls <211dbwls@naver.com>
2022-04-01 00:44:26 +0900

    과제10 MoreControls - CheckBox 배치

--- Task_10/app/src/main/java/com/example/morecontrols/MainActivity.java
+++ Task_10/app/src/main/java/com/example/morecontrols/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.morecontrols;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckBox(View view) {
+        CheckBox cb = (CheckBox) view;
+        Log.d(TAG, "Checked: " + cb.isChecked());
     }
 }
