9c24560863aea49a7bef80a0a4030e4e0e9c47e2
211dbwls <211dbwls@naver.com>
2022-04-01 21:57:23 +0900

    과제10 MoreControls - 파일 이름 수정

--- Task_10/app/src/main/java/com/example/morecontrols/MainActivity.java
+++ Task_10/app/src/main/java/com/example/morecontrols/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_main);
 
         cb = findViewById(R.id.checkbox);
         et = findViewById(R.id.nameEdit);
