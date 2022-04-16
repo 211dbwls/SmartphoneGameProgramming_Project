3e53ff47863c0aef1e8b4441e7d8852771f3bee4
211dbwls <211dbwls@naver.com>
2022-04-16 18:32:50 +0900

    Project - Stage1 블록 및 적 배치

--- Project/app/src/main/java/com/example/jellyking/game/MainGame.java
+++ Project/app/src/main/java/com/example/jellyking/game/MainGame.java
@@ -6,11 +6,15 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.view.MotionEvent;
 
+import com.example.jellyking.R;
 import com.example.jellyking.framework.BoxCollidable;
 import com.example.jellyking.framework.GameObject;
 import com.example.jellyking.framework.GameView;
 import com.example.jellyking.framework.Metrics;
 import com.example.jellyking.game.block.Block;
+import com.example.jellyking.game.block.BlockBroken;
+import com.example.jellyking.game.block.BlockJump;
+import com.example.jellyking.game.enemy.EnemyFix;
 
 import java.util.ArrayList;
 
@@ -31,6 +35,16 @@ public class MainGame {
     private JellyKing jellyKing;  // player.
 
     private Block block;  // block.
+    private float blockX, blockY;
+
+    private BlockBroken blockBroken;  // blockBroken.
+    private float blockBrokenX, blockBrokenY;
+
+    private BlockJump blockJump;  // blockJump.
+    private float blockJumpX, blockJumpY;
+
+    private EnemyFix enemyFix;  // enemyFix.
+    private float enemyFixX, enemyFixY;
 
     public float frameTime;
 
@@ -49,9 +63,137 @@ public class MainGame {
         jellyKing = new JellyKing(fx, fy);
         gameObjects.add(jellyKing);
 
-        /* 블록 추가 */
-        block = new Block(50, 50);
+        /* 블록 - 기본 */
+        for(int i = 0; i < 10; i++) {
+            blockX = Metrics.width / 26 * 2;  // x : 1
+            blockY = Metrics.height / 13 * 3 + (Metrics.height / 13 * (9 - i));
+            block = new Block(blockX, blockY);
+            gameObjects.add(block);
+        }
+
+        for(int i = 0; i < 4; i++) {
+            for(int j = 1; j <= 2; j++) {  // x : 1, 2
+                blockX = Metrics.width / 26 * (2 + j) ;
+                blockY = Metrics.height / 13 * 3  + (Metrics.height / 13 * (9 - i));
+                block = new Block(blockX, blockY);
+                gameObjects.add(block);
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 5; j <= 6; j++) {  // x : 5, 6
+                blockX = Metrics.width / 26 * (2 + j) ;
+                blockY = Metrics.height / 13 * 3  + (Metrics.height / 13 * (9 - i));
+                block = new Block(blockX, blockY);
+                gameObjects.add(block);
+            }
+        }
+
+        blockX = Metrics.width / 26 * (2 + 9) ;  // x : 9
+        blockY = Metrics.height / 13 * 3  + (Metrics.height / 13 * 9);
+        block = new Block(blockX, blockY);
         gameObjects.add(block);
+
+        for(int j = 10; j <= 19; j++) {
+            blockX = Metrics.width / 26 * (2 + j);  //x : 10 - 19
+            blockY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 6);  // y : 6
+            block = new Block(blockX, blockY);
+            gameObjects.add(block);
+        }
+
+        for(int i = 0; i < 6; i++) {
+            blockX = Metrics.width / 26 * (2 + 10);  // x : 10
+            blockY = Metrics.height / 13 * 3 + (Metrics.height / 13 * i);
+            block = new Block(blockX, blockY);
+            gameObjects.add(block);
+        }
+
+        blockX = Metrics.width / 26 * (2 + 14) ;  // 14
+        blockY = Metrics.height / 13 * 3  + (Metrics.height / 13 * 9);
+        block = new Block(blockX, blockY);
+        gameObjects.add(block);
+
+        for(int j = 19; j <= 21; j++) {  //x : 19, 20, 21, 21
+            blockX = Metrics.width / 26 * (2 + j);
+            blockY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 9);
+            block = new Block(blockX, blockY);
+            gameObjects.add(block);
+        }
+
+        for(int j = 18; j <= 21; j++) {
+            blockX = Metrics.width / 26 * (2 + j);  //x : 18 - 21
+            blockY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 3);  // y : 3
+            block = new Block(blockX, blockY);
+            gameObjects.add(block);
+        }
+
+        for(int i = 0; i < 10; i++) {
+            blockX = Metrics.width / 26 * (2 + 22);  // x : 22
+            blockY = Metrics.height / 13 * 3 + (Metrics.height / 13 * (9 - i));
+            block = new Block(blockX, blockY);
+            gameObjects.add(block);
+        }
+
+        /* 블록 - 부서지는 */
+        blockBrokenX = Metrics.width / 26 * (2 + 3);  // x : 3
+        blockBrokenY = Metrics.height / 13 * 3 + (Metrics.height / 13 * (9 - 2));
+        blockBroken = new BlockBroken(blockBrokenX, blockBrokenY);
+        gameObjects.add(blockBroken);
+
+        blockBrokenX = Metrics.width / 26 * (2 + 7);  // x : 7
+        blockBrokenY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 9);
+        blockBroken = new BlockBroken(blockBrokenX, blockBrokenY);
+        gameObjects.add(blockBroken);
+
+        for(int j = 10; j <= 13; j++) {
+            blockBrokenX = Metrics.width / 26 * (2 + j);  //x : 10 - 13
+            blockBrokenY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 9);
+            blockBroken = new BlockBroken(blockBrokenX, blockBrokenY);
+            gameObjects.add(blockBroken);
+        }
+
+        blockBrokenX = Metrics.width / 26 * (2 + 16);  // x : 16
+        blockBrokenY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 3);
+        blockBroken = new BlockBroken(blockBrokenX, blockBrokenY);
+        gameObjects.add(blockBroken);
+
+        /* 블록 - 점프 */
+        for(int j = 1; j <= 2; j++) {
+            blockJumpX = Metrics.width / 26 * (2 + j);  //x : 1, 2
+            blockJumpY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 5);
+            blockJump = new BlockJump(blockJumpX, blockJumpY);
+            gameObjects.add(blockJump);
+        }
+
+        blockJumpX = Metrics.width / 26 * (2 + 4);  //x : 4
+        blockJumpY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 7);
+        blockJump = new BlockJump(blockJumpX, blockJumpY);
+        gameObjects.add(blockJump);
+
+        blockJumpX = Metrics.width / 26 * (2 + 8);  //x : 8
+        blockJumpY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 9);
+        blockJump = new BlockJump(blockJumpX, blockJumpY);
+        gameObjects.add(blockJump);
+
+        /* 아이템 */
+
+        /* 적 */
+        for(int j = 3; j <= 4; j++) {
+            enemyFixX = Metrics.width / 26 * (2 + j);  //x : 3, 4
+            enemyFixY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 9);
+            enemyFix = new EnemyFix(enemyFixX, enemyFixY);
+            gameObjects.add(enemyFix);
+        }
+
+        enemyFixX = Metrics.width / 26 * (2 + 17);  //x : 17
+        enemyFixY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 3);
+        enemyFix = new EnemyFix(enemyFixX, enemyFixY);
+        gameObjects.add(enemyFix);
+
+        enemyFixX = Metrics.width / 26 * (2 + 18);  //x : 18
+        enemyFixY = Metrics.height / 13 * 3 + (Metrics.height / 13 * 9);
+        enemyFix = new EnemyFix(enemyFixX, enemyFixY);
+        gameObjects.add(enemyFix);
 
         /* 충돌 상자 */
         collisionPaint = new Paint();
--- Project/app/src/main/java/com/example/jellyking/game/block/BlockJump.java
+++ Project/app/src/main/java/com/example/jellyking/game/block/BlockJump.java
@@ -7,7 +7,7 @@ import com.example.jellyking.framework.Sprite;
 
 public class BlockJump extends Sprite {
     public BlockJump(float x, float y) {
-        super(x, y, R.dimen.block_radius, R.mipmap.block_jump_1);
+        super(x, y, R.dimen.block_radius, R.mipmap.block_jump_2);
     }
 
     public void draw(Canvas canvas) {
