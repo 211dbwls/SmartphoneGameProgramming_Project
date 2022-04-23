6df86b95687afe8bab72d38836bda30215d592fb
211dbwls <211dbwls@naver.com>
2022-04-23 16:31:27 +0900

    Project - Star boundingBox 생성 및 크기 변경

--- Project/app/src/main/java/com/example/jellyking/game/Star.java
+++ Project/app/src/main/java/com/example/jellyking/game/Star.java
@@ -1,13 +1,18 @@
 package com.example.jellyking.game;
 
 import android.graphics.Canvas;
+import android.graphics.RectF;
 
 import com.example.jellyking.R;
+import com.example.jellyking.framework.BoxCollidable;
+import com.example.jellyking.framework.Metrics;
 import com.example.jellyking.framework.Sprite;
 
-public class Star extends Sprite {
+public class Star extends Sprite implements BoxCollidable {
+    protected RectF boundingBox = new RectF();  // boundingBox
+
     public Star(float x, float y) {
-        super(x, y, R.dimen.block_radius, R.mipmap.star);
+        super(x, y, R.dimen.star_radius, R.mipmap.star);
     }
 
     public void draw(Canvas canvas) {
@@ -15,6 +20,13 @@ public class Star extends Sprite {
     }
 
     public void update() {
+        /* boundingBox */
+        float widthRadius = Metrics.size(R.dimen.star_radius) / 2;
+        boundingBox.set(x - widthRadius, y - widthRadius, x + widthRadius, y + widthRadius);
+    }
 
+    @Override
+    public RectF getBoundingRect() {
+        return boundingBox;
     }
 }
