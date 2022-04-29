e638dde61a0480ab65c20c4434868ccd39842baf
211dbwls <211dbwls@naver.com>
2022-04-29 16:48:12 +0900

    Project - MoveLRBlock 이동

--- Project/app/src/main/java/com/example/jellyking/game/block/BlockMoveLR.java
+++ Project/app/src/main/java/com/example/jellyking/game/block/BlockMoveLR.java
@@ -1,18 +1,29 @@
 package com.example.jellyking.game.block;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.RectF;
+import android.util.Log;
 
 import com.example.jellyking.R;
 import com.example.jellyking.framework.BoxCollidable;
 import com.example.jellyking.framework.Metrics;
 import com.example.jellyking.framework.Sprite;
+import com.example.jellyking.game.MainGame;
 
 public class BlockMoveLR extends Sprite implements BoxCollidable {
+    private static final String TAG = BlockMoveLR.class.getSimpleName();
+
     protected RectF boundingBox = new RectF();  // boundingBox
 
+    private float dx;
+
+    private float moveWidth = 0.0f;
+    private float moveWidthLimit = 970.0f;
+    boolean moveRight = true;
+
     public BlockMoveLR(float x, float y) {
-        super(x, y, R.dimen.block_radius, R.mipmap.block_3);
+        super(x, y, R.dimen.move_lr_block_width_radius, R.dimen.move_lr_block_height_radius, R.mipmap.block_3);
     }
 
     public void draw(Canvas canvas) {
@@ -20,9 +31,35 @@ public class BlockMoveLR extends Sprite implements BoxCollidable {
     }
 
     public void update() {
+        float frameTime = MainGame.getInstance().frameTime;
+
+        dx = Metrics.size(R.dimen.move_lr_block_speed);
+
+        float dx = this.dx * frameTime;
+
+        if(moveRight == true) {  // 오른쪽으로 이동하는 경우
+            if(moveWidth > moveWidthLimit) {  // 이동 거리 완주한 경우
+                moveRight = false;
+            }
+        }
+        else {
+            if(moveWidth < 0) {  // 이동 거리 완주한 경우
+                moveRight = true;
+            }
+            if(dx > 0) {
+                dx = -dx;
+            }
+        }
+
+        Log.d(TAG, "moveRight: " + moveRight + " moveWidth: " + moveWidth + " dx: " + dx);
+        dstRect.offset(dx, 0);
+        x += dx;
+        moveWidth += dx;
+
         /* boundingBox */
-        float widthRadius = Metrics.size(R.dimen.block_radius);
-        boundingBox.set(x - widthRadius, y - widthRadius, x + widthRadius, y);
+        float widthRadius = Metrics.size(R.dimen.move_lr_block_width_radius);
+        float heightRadius = Metrics.size(R.dimen.move_lr_block_height_radius);
+        boundingBox.set(x - widthRadius, y - heightRadius, x + widthRadius, y);
     }
 
     @Override
